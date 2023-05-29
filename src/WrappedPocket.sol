30f18ce47ea242a2fc9bdcfe05e60fbcf329c354
St4rgarden <stargardenbeats@gmail.com>
2023-05-29 12:03:36 -0700

    fee, fee collect, nonces and minting

--- src/WrappedPocket.sol
+++ src/WrappedPocket.sol
@@ -10,6 +10,20 @@ import "openzeppelin-contracts/contracts/token/ERC20/extensions/draft-ERC20Permi
 contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Permit {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 public constant BASIS_POINTS = 10000;
+
+    bool public feeFlag;
+    uint256 public feeBasis;
+    address public feeCollector;
+
+    mapping (address => uint256) private _userNonces;
+
+    event FeeSet(bool indexed flag, uint256 indexed newFeeBasis, address indexed feeCollector);
+    event FeeCollected(address indexed feeCollector, uint256 indexed amount);
+
+    error UserNonce(address user, uint256 nonce);
+    error FeeCollectorZero();
+    error FeeBasis();
 
     constructor() ERC20("Wrapped Pocket", "wPOKT") ERC20Permit("Wrapped Pocket") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -25,8 +39,36 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount, uint256 nonce) public onlyRole(MINTER_ROLE) {
+        uint256 currentNonce = _userNonces[to];
+        if (nonce != currentNonce + 1) {
+            revert UserNonce(to, nonce);
+        }
+        if (feeFlag == true) {
+            amount = _collectFee(amount);
+        }
+        _userNonces[to] = nonce;
         _mint(to, amount);
+    }
+
+    function _collectFee(uint256 amount) internal returns (uint256) {
+        if (amount % BASIS_POINTS != 0) {
+            revert FeeBasis();
+        }
+        uint256 fee = (amount * feeBasis) / BASIS_POINTS;
+        emit FeeCollected(feeCollector, fee);
+        _mint(feeCollector, fee);
+        return amount - fee;
+    }
+
+    function setFee(bool flag, uint256 newFeeBasis, address newFeeCollector) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (newFeeCollector == address(0)) {
+            revert FeeCollectorZero();
+        }
+        feeBasis = newFeeBasis;
+        feeFlag = flag;
+        feeCollector = newFeeCollector;
+        emit FeeSet(flag, newFeeBasis, newFeeCollector);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
@@ -35,5 +77,9 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
         override
     {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function getUserNonce(address user) public view returns (uint256) {
+        return _userNonces[user];
     }
 }
