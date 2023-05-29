3ce7e1c31ed8c06872b11f5c905d93f38c70c827
St4rgarden <stargardenbeats@gmail.com>
2023-05-29 14:04:16 -0700

    mint cooldown

--- src/MintController.sol
+++ src/MintController.sol
@@ -10,9 +10,16 @@ interface IWPokt {
 }
 
 contract MintController {
+
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
 
     IWPokt public immutable wPokt;
+    address public copper;
+
+    uint256 mintLimit = 28_500_000 ether;
+    uint256 mintPerSecond = 330;
+
+    event MintCooldownSet(uint256 newLimit, uint256 newCooldown);
 
     constructor(address _wPokt) {
         wPokt = IWPokt(_wPokt);
@@ -22,4 +29,28 @@ contract MintController {
         require(wPokt.hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "MintController: caller is not admin");
         _;
     }
+
+    modifier onlyCopper() {
+        require(msg.sender == copper, "MintController: caller is not copper");
+        _;
+    }
+
+    function setCopper(address _copper) external onlyAdmin {
+        copper = _copper;
+    }
+
+    function mintWrappedPocket(address recipient, uint256 amount, uint256 nonce) external onlyCopper {
+        wPokt.mint(recipient, amount, nonce);
+    }
+
+    function batchMintWrappedPocket(address[] calldata recipients, uint256[] calldata amounts, uint256[] calldata nonces) external onlyCopper {
+        wPokt.batchMint(recipients, amounts, nonces);
+    }
+
+    function setMintCooldown(uint256 newLimit, uint256 newCooldown) external onlyAdmin {
+        mintLimit = newLimit;
+        mintPerSecond = newCooldown;
+        emit MintCooldownSet(newLimit, newCooldown);
+    }
+
 }
