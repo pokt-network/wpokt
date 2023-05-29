76d90491a38d09d64b43aef4bafde48a1e70acaf
St4rgarden <stargardenbeats@gmail.com>
2023-05-29 12:23:39 -0700

    batchMint, burnAndBridge

--- src/WrappedPocket.sol
+++ src/WrappedPocket.sol
@@ -11,6 +11,7 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 public constant BASIS_POINTS = 10000;
+    uint256 public constant MAX_FEE_BASIS = 300;
 
     bool public feeFlag;
     uint256 public feeBasis;
@@ -20,10 +21,14 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
 
     event FeeSet(bool indexed flag, uint256 indexed newFeeBasis, address indexed feeCollector);
     event FeeCollected(address indexed feeCollector, uint256 indexed amount);
+    event BurnAndBridge(uint256 indexed amount, address indexed poktAddress, address indexed from);
 
     error UserNonce(address user, uint256 nonce);
     error FeeCollectorZero();
-    error FeeBasis();
+    error FeeBasisDust();
+    error BatchMintLength();
+    error MaxBasis();
+    error BlockBurn();
 
     constructor() ERC20("Wrapped Pocket", "wPOKT") ERC20Permit("Wrapped Pocket") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -37,6 +42,32 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
 
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
+    }
+
+    function batchMint(address[] memory to, uint256[] memory amount, uint256[] memory nonce) public onlyRole(MINTER_ROLE) {
+        if (to.length != amount.length || to.length != nonce.length) {
+            revert BatchMintLength();
+        }
+        for (uint256 i = 0; i < to.length; i++) {
+            _mintBatch(to[i], amount[i], nonce[i]);
+        }
+    }
+
+    function burnAndBridge(uint256 amount, address poktAddress) public {
+        _burn(msg.sender, amount);
+        emit BurnAndBridge(amount, poktAddress, msg.sender);
+    }
+
+    function _mintBatch(address to, uint256 amount, uint256 nonce) private {
+        uint256 currentNonce = _userNonces[to];
+        if (nonce != currentNonce + 1) {
+            revert UserNonce(to, nonce);
+        }
+        if (feeFlag == true) {
+            amount = _collectFee(amount);
+        }
+        _userNonces[to] = nonce;
+        _mint(to, amount);
     }
 
     function mint(address to, uint256 amount, uint256 nonce) public onlyRole(MINTER_ROLE) {
@@ -53,7 +84,7 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
 
     function _collectFee(uint256 amount) internal returns (uint256) {
         if (amount % BASIS_POINTS != 0) {
-            revert FeeBasis();
+            revert FeeBasisDust();
         }
         uint256 fee = (amount * feeBasis) / BASIS_POINTS;
         emit FeeCollected(feeCollector, fee);
@@ -61,14 +92,17 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
         return amount - fee;
     }
 
-    function setFee(bool flag, uint256 newFeeBasis, address newFeeCollector) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        if (newFeeCollector == address(0)) {
+    function setFee(bool flag, uint256 newFee, address newCollector) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (newCollector == address(0)) {
             revert FeeCollectorZero();
         }
-        feeBasis = newFeeBasis;
+        if (newFee > MAX_FEE_BASIS) {
+            revert MaxBasis();
+        }
+        feeBasis = newFee;
         feeFlag = flag;
-        feeCollector = newFeeCollector;
-        emit FeeSet(flag, newFeeBasis, newFeeCollector);
+        feeCollector = newCollector;
+        emit FeeSet(flag, newFee, newCollector);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
@@ -77,6 +111,10 @@ contract WrappedPocket is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Pe
         override
     {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function burn(uint256) public pure override {
+        revert BlockBurn();
     }
 
     function getUserNonce(address user) public view returns (uint256) {
