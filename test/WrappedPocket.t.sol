738163e5d2cc1bcbc9c9e2a9010986a40badaf4e
St4rgarden <stargardenbeats@gmail.com>
2023-06-26 13:22:43 -0700

    test basic

--- test/WrappedPocket.t.sol
+++ test/WrappedPocket.t.sol
@@ -6,8 +6,34 @@ import "../src/WrappedPocket.sol";
 
 contract WrappedPocketTest is Test {
 
+    WrappedPocket public wPokt;
+
     function setUp() public {
-        WrappedPocket wPokt = new WrappedPocket();
+        wPokt = new WrappedPocket();
+    }
+
+    function testTokenName() public {
+        string memory expected = "Wrapped Pocket";
+        string memory actual = wPokt.name();
+        assertEq(actual, expected);
+    }
+
+    function testTokenSymbol() public {
+        string memory expected = "wPOKT";
+        string memory actual = wPokt.symbol();
+        assertEq(actual, expected);
+    }
+
+    function testTokenDecimals() public {
+        uint256 expected = 6;
+        uint256 actual = wPokt.decimals();
+        assertEq(actual, expected);
+    }
+
+    function testTokenDecimalsNot18() public {
+        uint256 expected = 18;
+        uint256 actual = wPokt.decimals();
+        assertNotEq(actual, expected);
     }
 
 }
